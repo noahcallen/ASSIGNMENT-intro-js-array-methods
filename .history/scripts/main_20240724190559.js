96f57013e3df6176b958499b49f8bac1487f413f
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2024-07-24 20:56:28 -0400

    I FINALLY GOT THIS WORKING

--- .history/scripts/main_20240724190559.js
+++ .history/scripts/main_20240724190559.js
@@ -6,12 +6,7 @@ import { renderToDom } from "../utils/renderToDom.js";
 // Reusable function to get the cards on the DOM
 // .forEach()
 const renderCards = (array) => {
-  let refStuff = "";
-
-array.forEach((item) => {
-  refStuff += card(item);
-});
-
+  let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
   renderToDom("#cards", refStuff);
 }
 
@@ -19,15 +14,7 @@ array.forEach((item) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-    // GOOGLE: Destructuring assignment
-   const [, id] = event.target.id.split('--');
-
-   const index =referenceList.findIndex(item => item.id === Number(id));
-
-   //Toggle Update the value inCart
-   referenceList[index].inCart = !referenceList[index].inCart
-   cartTotal();
-   renderCards(referenceList);
+   console.log('Clicked Fav btn')
   }
 }
 
@@ -35,30 +22,23 @@ const toggleCart = (event) => {
 // .filter()
 const search = (event) => {
   const eventLC = event.target.value.toLowerCase();
-  const searchResult = referenceList.filter(item =>
-    item.title.toLowerCase().includes(eventLC) ||
-    item.author.toLowerCase().includes(eventLC) ||
-    item.description.toLowerCase().includes(eventLC)
-  );
-  renderCards(searchResult);
+  console.log(eventLC)
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    const free = referenceList.filter(item => item.price <= 0);
-    renderCards(free);
+    console.log('FREE')
   }
   if(event.target.id.includes('cartFilter')) {
-    const cart = referenceList.filter(item => item.inCart);
-    renderCards(cart);
+    console.log('cartFilter')
   }
   if(event.target.id.includes('books')) {
-    const books = referenceList.filter(item => item.type.toLowerCase() === 'book');
+    console.log('books!')
   }
   if(event.target.id.includes('clearFilter')) {
-    renderCards(referenceList);
+    console.log('clearFilter')
   }
   if(event.target.id.includes('productList')) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
@@ -71,37 +51,29 @@ const buttonFilter = (event) => {
     </thead>
     <tbody>
     `;
-  //localeCompare: compares where it is before or after; This ex is seeing if a or b is first returning 0 or 1
-  productList().sort((a, b) => a.type.localeCompare(b.type))
-  .forEach(item => {
-    table += tableRow(item);
-  })
-  table += `</tbody></table>`
+    
+    productList().forEach(item => {
+      table += tableRow(item);
+    });
 
-  renderToDom('#cards', table);
-}
+    table += `</tbody></table>`
+
+    renderToDom('#cards', table);
+  }
   
 }
 
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const cart = referenceList.filter(item => item.inCart);
-  const total = cart.reduce((a,b) => a + b.price, 0); //0 is the intitual value, this actually adds the total of the cart (doesnt reset)
-  const freeItems = cart.some(item => item.price <= 0)
+  const total = 0
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
-
-  if (freeItems) {
-    document.querySelector("#includes-free").innerHTML = 'Includes Free Items';
-  }else{
-    document.querySelector("#includes-free").innerHTML = '';
-  }
 }
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return referenceList.map(item => ({title: item.title, price: item.price, type: item.type}));
+  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
 }
 
 
